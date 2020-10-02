66275c46be1a1a294dd75312f63252cb99e9093d
hyemin <hyemin0723@swu.ac.kr>
2020-10-02 21:24:35 +0900

    hyemin_search

--- app/src/main/java/com/example/corona/MainActivity.java
+++ app/src/main/java/com/example/corona/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.corona;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //검색
+        SearchView searchView;
+        searchView = findViewById(R.id.main_search);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //검색 버튼이 눌러졌을 때 이벤트 처리
+                Toast.makeText(MainActivity.this,"검색 처리됨 : "+query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //검색어가 변경되었을 때 이벤트 처리
+                return false;
+            }
+        });
     }
+
    }
