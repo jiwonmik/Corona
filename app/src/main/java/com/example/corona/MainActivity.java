971818364afaf4572193e71c7467f64046eedeb8
hyemin <hyemin0723@swu.ac.kr>
2020-10-03 00:07:15 +0900

    hyemin_merge

--- app/src/main/java/com/example/corona/MainActivity.java
+++ app/src/main/java/com/example/corona/MainActivity.java
@@ -6,49 +6,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SearchView;
-import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final int sub = 1001; /*다른 액티비티를 띄우기 위한 요청코드(상수)*/
+    private Button MapButton;
+    private Button ChatButton;
+    private Button WebButton;
+    private Button location_Btn;
+
+//    public static final int sub = 1001; /*다른 액티비티를 띄우기 위한 요청코드(상수)*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //검색
-        SearchView searchView;
-        searchView = findViewById(R.id.main_search);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //검색 버튼이 눌러졌을 때 이벤트 처리
-                Toast.makeText(MainActivity.this,"검색 처리됨 : "+query, Toast.LENGTH_SHORT).show();
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //검색어가 변경되었을 때 이벤트 처리
-                return false;
-            }
-        });
-
+        //버튼 이벤트
+        MapButton = (Button)findViewById(R.id.map_button);
+        ChatButton = (Button)findViewById(R.id.chat_button);
+        WebButton = (Button)findViewById(R.id.web_button);
         //최근 확진자 발견 위치 버튼
-        Button location_Btn = (Button) findViewById(R.id.location_btn);
-        location_Btn.setOnClickListener(new Button.OnClickListener(){
+        location_Btn = (Button)findViewById(R.id.location_btn);
 
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LocationActivity.class);
-                startActivityForResult(intent, sub);
-            }
-        });
+        MapButton.setOnClickListener(this);
+        ChatButton.setOnClickListener(this);
+        WebButton.setOnClickListener(this);
+        location_Btn.setOnClickListener(this);
+          //검색
+//        final SearchView searchView = (SearchView)findViewById(R.id.main_search);
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                //검색 버튼이 눌러졌을 때 이벤트 처리-> 검색시 그 위치정보를 지도로 보일 수 잇어야함
+//                return true;
+//            }
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                //검색어가 변경되었을 때 이벤트 처리
+//                return true;
+//            }
+//        });
+    }
 
-   }
+    //버튼(map, chat수정해야함)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.map_button:
+                startActivity(new Intent(getApplicationContext(),WebActivity.class));
+                break;
+            case R.id.chat_button:
+                startActivity(new Intent(getApplicationContext(),WebActivity.class));
+                break;
+            case R.id.web_button:
+                startActivity(new Intent(getApplicationContext(),WebActivity.class));
+                break;
+            case R.id.location_btn:
+                startActivity(new Intent(this,LocationActivity.class));
+                break;
+
+//                Intent intent = new Intent(getApplicationContext(),LocationActivity.class);
+//                startActivityForResult(intent, sub);
+            default:
+                break;
+        }
+    }
 
    }
--- app/src/main/java/com/example/corona/WebActivity.java
+++ app/src/main/java/com/example/corona/WebActivity.java
@@ -1,18 +1,33 @@
 package com.example.corona;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class WebActivity extends Activity {
     private WebView webView;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web_corona);
+
+        //타이틀 클릭시 mainactivity로 이동
+        textView = (TextView)findViewById(R.id.map_main_button);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
 
         webView = (WebView)findViewById(R.id.webView);
         /*웹 세팅 작업하기*/
@@ -30,6 +45,7 @@ public class WebActivity extends Activity {
         /*웹 뷰 띄우기*/
         webView.loadUrl("http://ncov.mohw.go.kr/");
     }
+
     public void onBackPressed(){
         if(webView.canGoBack()) webView.goBack();
         else finish();
