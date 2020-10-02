a5e7faf9a1376112942341a28a8a6737bedd3957
hyemin <hyemin0723@swu.ac.kr>
2020-10-02 16:59:28 +0900

    hyemin_submain and title

--- app/src/main/java/com/example/corona/MainActivity.java
+++ app/src/main/java/com/example/corona/MainActivity.java
@@ -2,41 +2,16 @@ package com.example.corona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.web_corona);
-
-        webView = (WebView)findViewById(R.id.webView);
-        /*웹 세팅 작업하기*/
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setAllowFileAccessFromFileURLs(true);
-        webSettings.setSaveFormData(false);
-        webSettings.setSavePassword(false);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        /*리다이렉트 할 때 브라우저 열리는 것 방지*/
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
-        /*웹 뷰 띄우기*/
-        webView.loadUrl("http://ncov.mohw.go.kr/");
+        setContentView(R.layout.activity_main);
     }
-    public void onBackPressed(){
-        if(webView.canGoBack()) webView.goBack();
-        else finish();
-    }
-
-}
+   }
--- app/src/main/java/com/example/corona/SubMainActivity.java
+++ app/src/main/java/com/example/corona/SubMainActivity.java
@@ -3,21 +3,20 @@ package com.example.corona;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class SubMainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.first_main);
 
-        Handler hand = new Handler();
-        hand.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                finish();
-            }
-        }, 2000);
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
+
 }
